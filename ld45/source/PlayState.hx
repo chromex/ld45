151a596ceb2341888eb369d481574438f0922422
Loren Hoffman <loren.hoffman@gmail.com>
2019-10-05 16:16:51 -0700

    adding_basic_map

--- ld45/source/PlayState.hx
+++ ld45/source/PlayState.hx
@@ -1,7 +1,11 @@
 package;
 
+import haxe.display.Position.Range;
+import flixel.FlxObject;
+import flixel.tile.FlxTilemap;
 import flixel.math.FlxRect;
 import lime.math.Rectangle;
+import flixel.addons.editors.ogmo.FlxOgmoLoader;
 import flixel.FlxCamera;
 import flixel.FlxG;
 import flixel.FlxSprite;
@@ -20,12 +24,13 @@ class PlayState extends FlxState {
 
 	var rng:FlxRandom;
 
+	var terrain:FlxTilemap;
+	var water:FlxTilemap;
+
 	override public function create():Void {
 		super.create();
 
-		_player = new Odin(100, 60);
-		// Adds the player to the state
-		add(_player);
+		LoadMap();
 
 		FlxG.camera.zoom = 3;
 		FlxG.camera.follow(_player, FlxCameraFollowStyle.TOPDOWN_TIGHT, GameConstants.CameraLerp);
@@ -54,5 +59,30 @@ class PlayState extends FlxState {
 		super.update(elapsed);
 
 		FlxG.collide(followers, _player);
+	}
+
+	private function PlaceEntities(entityName:String, entityData:Xml):Void {
+		var px = entityData.get("x");
+		var py = entityData.get("y");
+
+		if (entityName == "player")
+		{
+			_player = new Odin(Std.parseInt(px), Std.parseInt(py));
+			add(_player);
+		}
+	}
+
+	private function LoadMap():Void {
+		var map:FlxOgmoLoader = new FlxOgmoLoader(AssetPaths.island2__oel);
+		water = map.loadTilemap(AssetPaths.water__png, 16, 16, "water");
+		terrain = map.loadTilemap(AssetPaths.tileset__png, 16, 16, "terrain");
+		terrain.follow();
+		// terrain.setTileProperties(0, FlxObject.NONE);
+		// terrain.setTileProperties(5, FlxObject.ANY, null, null, 5);
+		// terrain.setTileProperties(10, FlxObject.NONE, null, null, 2);
+		add(water);
+		add(terrain);
+
+		map.loadEntities(PlaceEntities, "entities");
 	}
 }
