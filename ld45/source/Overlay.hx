ac13aaa2e9e5f99144bc4f589dc2ed5a72024c08
Loren Hoffman <loren.hoffman@gmail.com>
2019-10-06 14:56:00 -0700

    add start skreen

--- ld45/source/Overlay.hx
+++ ld45/source/Overlay.hx
@@ -2,23 +2,31 @@ package;
 
 import flixel.math.FlxPoint;
 import flixel.FlxG;
-import flixel.tweens.FlxEase;
-import flixel.tweens.FlxTween;
 import flixel.FlxSprite;
 import flixel.group.FlxGroup;
 
 class Overlay extends FlxGroup {
 	public var overlay:FlxSprite;
+	public var title:FlxSprite;
     public var position:FlxPoint;
 
 	public function new() {
 		super();
         position = new FlxPoint(-1000,-1000);
 		overlay = new FlxSprite(position.x, position.y, "assets/sprites/overlay.png");
+		title = new FlxSprite(-440, -760, "assets/sprites/FOO_Title.png");
+		title.scale.set(4,4);
+		title.alpha = 0.85;
+
 		add(overlay);
+		add(title);
 	}
 
 	override public function update(elapsed:Float):Void {
 		super.update(elapsed);
+
+		if (FlxG.keys.anyJustPressed([W,A,S,D,UP,DOWN,LEFT,RIGHT,SPACE]) || FlxG.mouse.justPressed) {
+			remove(title);
+		}
 	}
 }
